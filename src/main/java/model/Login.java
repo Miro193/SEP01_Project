33d0100efdb3692cc526e8c235549c50b282e127
Fari-panah <panahfari81@gmail.com>
2025-09-08 12:36:54 +0300

    Login model class

---
+++ src/main/java/model/Login.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Login {
+    private String username;
+    private String password;
+
+    public Login(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getters and setters
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // Optional: method to show login details (without exposing password)
+    @Override
+    public String toString() {
+        return "Login{username='" + username + "'}";
+    }
+}
