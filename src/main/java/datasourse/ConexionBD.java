eb143de74b8d07ab155be1e75e14faab576c35a4
michabl116 <michabl@metropolia.fi>
2025-09-10 14:19:09 +0300

    add datasourse

---
+++ src/main/java/datasourse/ConexionBD.java
@@ -0,0 +1,22 @@
+package datasourse;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionBD {
+    private static final String URL = "jdbc:mariadb://localhost:3306/studyplanner";
+    private static final String USER = "root";
+    private static final String PASSWORD = "admin";
+
+    public static Connection obtenerConexion() {
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+}
