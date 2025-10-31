0ed29db24df3481fb1b982065728665bc7273793
Miro193 <mirostonezone@gmail.com>
2025-10-31 20:41:41 +0200

    testii haku ResourceBundle

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -7,23 +7,36 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 import model.CurrentUser;
 import model.User;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class LoginController {
-    @FXML
-    private TextField usernameField;
+    @FXML private Label headerLogin;
+    @FXML private Label labelUsername;
+    @FXML private TextField usernameField;
+    @FXML private Label labelPassword;
+    @FXML private PasswordField passwordField;
+    @FXML private Button btnLogin;
+    @FXML private Button btnSignup;
+    @FXML private MenuButton btnLanguage;
+    @FXML private MenuItem itemPersian;
+    @FXML private MenuItem itemFinnish;
+    @FXML private MenuItem itemEnglish;
 
-    @FXML
-    private PasswordField passwordField;
 
     private UserDao userDao = new UserDao();
+    private ResourceBundle rb;
+
+    @FXML
+    public void initialize() {
+        handleLanguage("fa", "IR");
+    }
 
     @FXML
     private void handleLogin(ActionEvent event) throws IOException {
@@ -69,5 +82,26 @@ public class LoginController {
         alert.setTitle(title);
         alert.setContentText(message);
         alert.showAndWait();
+    }
+    @FXML
+    private void handleLanguage(String language, String country) {
+        Locale locale = new Locale(language, country);
+        rb = ResourceBundle.getBundle("MessagesBundle", locale);
+        headerLogin.setText(rb.getString("headerLogin.text"));
+        labelUsername.setText(rb.getString("labelUsername.text"));
+        labelPassword.setText(rb.getString("labelPassword.text"));
+        btnLogin.setText(rb.getString("btnLogin.text"));
+        btnSignup.setText(rb.getString("btnSignup.text"));
+        btnLanguage.setText(rb.getString("btnLanguage.text"));
+        itemPersian.setText(rb.getString("itemPersian.text"));
+    }
+
+    public void onPersianClick(ActionEvent event) {
+    }
+
+    public void onFinnishClick(ActionEvent event) {
+    }
+
+    public void onEnlishClick(ActionEvent event) {
     }
 }
