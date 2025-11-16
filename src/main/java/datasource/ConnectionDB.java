0051b5814b53474b49ae1464f824d6a0aaea14d9
amirsaeid93 <said.taebnia@gmail.com>
2025-11-16 16:10:48 +0200

    Update

--- src/main/java/datasource/ConnectionDB.java
+++ src/main/java/datasource/ConnectionDB.java
@@ -1,32 +1,16 @@
-/*package datasource;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-public class ConnectionDB {
-    private static final String URL = "jdbc:mariadb://host.docker.internal:3306/StudyPlanner";
-    //private static final String URL = "jdbc:mariadb://localhost:3306/studyplanner";
-    private static final String USER = "root";
-    private static final String PASSWORD = "root";
-
-    public static Connection obtenerConexion() {
-        try {
-            Class.forName("org.mariadb.jdbc.Driver");
-            return DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-}
-*/
 package datasource;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Logger;
 
 public class ConnectionDB {
+    private static final Log log = LogFactory.getLog(ConnectionDB.class);
+
     public static Connection obtenerConexion() {
         try {
             Class.forName("org.mariadb.jdbc.Driver");
@@ -36,18 +20,20 @@ public class ConnectionDB {
                 host = "localhost"; // fallback
             }
 
-            String url = "jdbc:mariadb://" + host + ":3306/Studyplanner";
+            String url = "jdbc:mariadb://" + host + ":3306/StudyplannerSimple";
             String user = "root";
-            String password = "admin";
+            String password = "root";
 
-            System.out.println("Connecting to: " + url);
+            Logger log = Logger.getLogger("ConnectionDB");
+
+            log.info("Connecting to " + url);
             Connection conn = DriverManager.getConnection(url, user, password);
-            System.out.println("Connection established: " + (conn != null));
+            log.info("Connection established: " + (conn != null));
             return conn;
         } catch (ClassNotFoundException e) {
-            System.err.println("Driver not found: " + e.getMessage());
+            log.error("Driver not found: " + e.getMessage());
         } catch (SQLException e) {
-            System.err.println("Connection error: " + e.getMessage());
+            log.error("Connection error: " + e.getMessage());
         }
         return null;
     }
