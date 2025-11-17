f568a39ae3694ac8ec45afc26e7d047d093fda82
Michel <michabl@metropolia.com>
2025-11-17 10:43:29 +0200

    update

--- src/main/java/utils/LanguageManager.java
+++ src/main/java/utils/LanguageManager.java
@@ -1,28 +1,20 @@
 package utils;
 
-import java.util.HashMap;
 import java.util.Locale;
-//import java.util.ResourceBundle;
 import dao.LocalizationDao;
 import java.util.Map;
 
 public class LanguageManager {
 
     private static Locale currentLocale = new Locale("en", "US");
-   // private static ResourceBundle bundle = ResourceBundle.getBundle("MessagesBundle", currentLocale);
     private static Map<String, String> localizedStrings;
     private static LocalizationDao localizationDao = new LocalizationDao();
 
     public static void setLanguage(String language, String country) {
         currentLocale = new Locale(language, country);
-        //bundle = ResourceBundle.getBundle("MessagesBundle", currentLocale);
         localizedStrings = localizationDao.getLocalizedStrings(currentLocale);
-
     }
 
-   /* public static ResourceBundle getBundle() {
-        return bundle;
-    }*/
    public static Map<String, String> getLocalizedStrings() {
        return localizedStrings;
    }
@@ -30,12 +22,10 @@ public class LanguageManager {
     public static Locale getCurrentLocale() {
         return currentLocale;
     }
-    //in localizedStrings map searchs with key:
     public static String getTranslation(String key) {
         if (localizedStrings == null) {
             setLanguage("en", "US");
         }
-
         if (!localizedStrings.containsKey(key)) {
             System.out.println("Translation key" +key+" not found for language: "+currentLocale.getLanguage());
 
