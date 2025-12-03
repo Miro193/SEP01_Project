5e80f946c705759731b174592989ffde630d49cd
Michel <michabl@metropolia.com>
2025-12-03 23:51:32 +0200

    update

--- src/test/java/controller/SignUpControllerTest.java
+++ src/test/java/controller/SignUpControllerTest.java
@@ -5,8 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import model.User;
 import org.junit.jupiter.api.extension.ExtendWith;
