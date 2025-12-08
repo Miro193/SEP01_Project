b0cc9c1fc535c8425b59c30377c4a1b4a2ad4dfb
faripanah <farnaz.jamilpanah@metropolia.fi>
2025-12-08 17:21:57 +0200

    added lego in titles of pages and updated titles

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -5,6 +5,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
+import javafx.scene.image.Image;
 
 
 public class Main extends Application {
@@ -12,7 +13,9 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/Login.fxml"));
-        primaryStage.setTitle("Login");
+        primaryStage.setTitle("Study Planner");
+        Image legoIcon = new Image(getClass().getResourceAsStream("/studying-icon.png"));
+        primaryStage.getIcons().add(legoIcon);
         primaryStage.setScene(new Scene(root, 400, 250));
         primaryStage.show();
     }
